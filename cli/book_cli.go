430dda04b712c4b453f59b111f010c0f576d55a9
jean-silva-dev <jean.silva@solinftec.com>
2021-10-25 18:01:36 -0300

    Adição de verificações necessárias.

--- cli/book_cli.go
+++ cli/book_cli.go
@@ -49,6 +49,7 @@ func insert() {
 	be := createBookEntity()
 	buc := factorymethod.CreateBookUseCase()
 	buc.CreateBook(be)
+	log.Println("Created with successful.")
 }
 
 func read() {
@@ -74,87 +75,93 @@ func read() {
 func update() {
 	books := getAllBooks()
 
-	for {
-		fmt.Println("")
-		fmt.Println("What is the item that you wish to change?")
-		fmt.Println("")
-		fmt.Println("----------")
-		for index, value := range books {
-			fmt.Printf("[%d]\n", index+1)
-			fmt.Printf("Actor name: %s. \n", value.Author)
-			fmt.Printf("Title name: %s. \n", value.Title)
-			fmt.Printf("Publication year: %d. \n", value.Year)
+	if len(books) == 0 {
+		log.Println("None data was found on database.")
+	} else {
+
+		for {
+			fmt.Println("")
+			fmt.Println("What is the item that you wish to change?")
+			fmt.Println("")
 			fmt.Println("----------")
+			for index, value := range books {
+				fmt.Printf("[%d]\n", index+1)
+				fmt.Printf("Actor name: %s. \n", value.Author)
+				fmt.Printf("Title name: %s. \n", value.Title)
+				fmt.Printf("Publication year: %d. \n", value.Year)
+				fmt.Println("----------")
+			}
+			var answer int
+			fmt.Scanln(&answer)
+
+			if answer <= 0 {
+				log.Println("Please, select a right value.")
+				continue
+			}
+
+			if answer > len(books) {
+				log.Println("Please, select a right value.")
+				continue
+			}
+
+			index := answer - 1
+			element := books[index]
+			buc := factorymethod.CreateBookUseCase()
+			indexForChange := buc.FindByNameBook(element.Title)
+
+			fmt.Println("")
+			fmt.Println("Do insert new dates, please.")
+			fmt.Println("")
+			be := createBookEntity()
+
+			buc.UpdateBook(be, indexForChange)
+			log.Println("Updated with successful.")
+			break
 		}
-		var answer int
-		fmt.Scanln(&answer)
-
-		if answer <= 0 {
-			log.Println("Please, select a right value.")
-			continue
-		}
-
-		if answer > len(books) {
-			log.Println("Please, select a right value.")
-			continue
-		}
-
-		index := answer - 1
-		element := books[index]
-		buc := factorymethod.CreateBookUseCase()
-		indexForChange := buc.FindByNameBook(element.Title)
-
-		var title string
-		var actor string
-		var year int
-		fmt.Println("Do insert new dates, please.")
-		fmt.Print("Title name:")
-		fmt.Scanln(&title)
-		fmt.Print("Actor name:")
-		fmt.Scanln(&actor)
-		fmt.Print("Publication year:")
-		fmt.Scanln(&year)
-
-		buc.UpdateBook(title, actor, year, indexForChange)
-		break
 	}
 }
 
 func delete() {
 	books := getAllBooks()
 
-	for {
-		fmt.Println("")
-		fmt.Println("What is the item that you wish to delete?")
-		fmt.Println("")
-		fmt.Println("----------")
-		for index, value := range books {
-			fmt.Printf("[%d]\n", index+1)
-			fmt.Printf("Actor name: %s. \n", value.Author)
-			fmt.Printf("Title name: %s. \n", value.Title)
-			fmt.Printf("Publication year: %d. \n", value.Year)
+	if len(books) == 0 {
+		log.Println("None data was found on database.")
+	} else {
+
+		for {
+			fmt.Println("")
+			fmt.Println("What is the item that you wish to delete?")
+			fmt.Println("")
 			fmt.Println("----------")
+			for index, value := range books {
+				fmt.Printf("[%d]\n", index+1)
+				fmt.Printf("Actor name: %s. \n", value.Author)
+				fmt.Printf("Title name: %s. \n", value.Title)
+				fmt.Printf("Publication year: %d. \n", value.Year)
+				fmt.Println("----------")
+			}
+			var answer int
+			fmt.Scanln(&answer)
+
+			if answer <= 0 {
+				log.Println("Please, select a right value.")
+				continue
+			}
+
+			if answer > len(books) {
+				log.Println("Please, select a right value.")
+				continue
+			}
+
+			index := answer - 1
+			element := books[index]
+			buc := factorymethod.CreateBookUseCase()
+			indexForChange := buc.FindByNameBook(element.Title)
+
+			buc.DeleteBook(indexForChange)
+			log.Println("Removed with successful.")
+			break
 		}
-		var answer int
-		fmt.Scanln(&answer)
-
-		if answer <= 0 {
-			log.Println("Please, select a right value.")
-			continue
-		}
-
-		if answer > len(books) {
-			log.Println("Please, select a right value.")
-			continue
-		}
-
-		index := answer - 1
-		element := books[index]
-		buc := factorymethod.CreateBookUseCase()
-		indexForChange := buc.FindByNameBook(element.Title)
-
-		buc.DeleteBook(indexForChange)
-		break
 	}
 }
 
